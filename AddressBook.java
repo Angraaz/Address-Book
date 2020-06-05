bfe4108fa61c5038331d8338983e26c2f674c9d3
Angraaz <63395289+Angraaz@users.noreply.github.com>
2020-06-05 12:55:57 +0530

    Update AddressBook.java

--- AddressBook.java
+++ AddressBook.java
@@ -10,69 +10,79 @@ interface IAddressBook
 	public void sortAddressBookByZip();
 	public void sortAddressBookByLastName();
 	public void printAddressBook();
+	public void addressBookMenu();
 }
 class AddressBook implements IAddressBook
 {
-	private ArrayList<Person> addressBook;
+	ArrayList<Person> addressBook=new ArrayList<Person>();
 	Scanner takeInput=new Scanner(System.in);
 	public void addPerson()
-	{
+	{	
+		System.out.print("\n\t\t   Details of Individuals\n\n");
 		Person currentPerson=new Person();
 		addressBook.add(currentPerson);
 	}
 	public void editPerson() 
 	{
-		System.out.println("Enter the ID of entry to edit :");
-		int searchId=takeInput.nextInt();
+		System.out.print("\n\t\t   Enter the ID of entry to edit :");
+		int searchId=Integer.parseInt(takeInput.nextLine());
 		for(int i=0;i<addressBook.size();i++)
 		{	
-			Person currentPerson=addressBook.get(i);
-			if(currentPerson.getId() == searchId)
+			
+			if(addressBook.get(i).getId() == searchId)
 			{
-			System.out.println("Enter 1 to edit PhoneNumber\n"
-					          +"Enter 2 to edit Street Address\n"
-					          +"Enter 3 to edit City\n"
-					          +"Enter 4 to edit State\n"
-					          +"Enter 5 to edit zip code\n");
-			int choice=takeInput.nextInt();
-			switch (choice)
-			{
-				case 1:
-					currentPerson.setPhoneNumber();
-				break;
-				case 2:
-					currentPerson.completeAddress.setAddress();
-				break;
-				case 3:
-					currentPerson.completeAddress.setCity();
-				break;
-				case 4:
-					currentPerson.completeAddress.setState();
-				break;
-				case 5:
-					currentPerson.completeAddress.setZipcode();
-				break;
-				default:
-					System.out.println("INVALID INPUT");
-			}		
-			}
-			}
-		}
-
-	public void deletePerson() 
-	{
-		System.out.println("Enter the ID of entry to delete :");
-		int searchId=takeInput.nextInt();
-		for(int i=0;i<addressBook.size();i++)
-		{	
-			Person currentPerson=new Person();
-			if(currentPerson.getId() == searchId)
-			{
-				addressBook.remove(i);
+				System.out.print("\n\t\t    1 --> edit PhoneNumber\n"
+					          +"\t\t    2 --> edit Street Address\n"
+					          +"\t\t    3 --> edit City\n"
+					          +"\t\t    4 --> edit State\n"
+					          +"\t\t    5 --> edit zip code\n");
+				System.out.print("\n\t\t    ENTER YOUR CHOICE :");
+				int choice=Integer.parseInt(takeInput.nextLine());
+				switch (choice)
+				{
+					case 1:
+						System.out.print("\n\n\t\t    Enter new Phone Number :");
+						addressBook.get(i).setPhoneNumber(Long.parseLong(takeInput.nextLine()));
+					break;
+					case 2:
+						System.out.print("\n\n\t\t    Enter new H.No. and Area :");
+						addressBook.get(i).completeAddress.setAddress(takeInput.nextLine());
+					break;
+					case 3:
+						System.out.print("\n\n\t\t    Enter new City :");
+						addressBook.get(i).completeAddress.setCity(takeInput.nextLine());
+					break;
+					case 4:
+						System.out.print("\n\n\t\t    Enter new State :");
+						addressBook.get(i).completeAddress.setState(takeInput.nextLine());
+					break;
+					case 5:
+						System.out.print("\n\n\t\t    Enter new Zipcode :");
+						addressBook.get(i).completeAddress.setZipcode(Integer.parseInt(takeInput.nextLine()));
+					break;
+					default:
+						System.out.print("\n\n\t\t    INVALID INPUT");
+				}		
 			}
 		}
 	}
-
+	public void deletePerson() 
+	{	
+		if(addressBook.size()>0)
+		{
+			System.out.print("\n\t\t    Enter the ID of entry to delete :");
+			int searchId=Integer.parseInt(takeInput.nextLine());
+			for(int i=0;i<addressBook.size();i++)
+			{	
+				if(addressBook.get(i).getId() == searchId)
+				{
+					addressBook.remove(i);
+				}
+			}
+		}
+		else
+			System.out.print("\n\t\t    NO ENTRY TO DELETE");
+	}
 	public void sortAddressBookByZip() 
 	{
 		for (int i=0;i<addressBook.size();i++)
@@ -86,9 +96,9 @@ class AddressBook implements IAddressBook
 				else if(addressBook.get(j-1).completeAddress.zipcode == addressBook.get(j).completeAddress.zipcode)
 				{
 					if(addressBook.get(j-1).firstName.compareTo(addressBook.get(j).firstName)>0)
-							{
+					{
 								Collections.swap(addressBook, j-1, j);
-							}
+					}
 				}
 			}
 		}
@@ -97,13 +107,13 @@ class AddressBook implements IAddressBook
 	{
 		for (int i=0;i<addressBook.size();i++)
 		{
-			for (int j=1;j<addressBook.size()-i;j++)
+			for (int j=1;j<addressBook.size()-i;j++) 
 			{
-				if(addressBook.get(j-1).lastName.compareTo(addressBook.get(j).lastName)>0)
+				if(addressBook.get(j-1).lastName.compareToIgnoreCase((addressBook.get(j).lastName))>0)
 				{
 					Collections.swap(addressBook, j-1, j);
 				}
-				else if(addressBook.get(j-1).completeAddress.zipcode == addressBook.get(j).completeAddress.zipcode)
+				else if(addressBook.get(j-1).lastName.compareToIgnoreCase((addressBook.get(j).lastName))==0)
 				{
 					if(addressBook.get(j-1).firstName.compareTo(addressBook.get(j).firstName)>0)
 							{
@@ -114,11 +124,72 @@ class AddressBook implements IAddressBook
 		}
 		
 	}
-	public void printAddressBook() {
-		
-		for (int i=0;i<addressBook.size();i++)
+	public void printAddressBook() 
+	{
+		if(addressBook.size()>0)
+		{	
+			for (int i=0;i<addressBook.size();i++)
+			{
+				System.out.println("\n"+addressBook.get(i));
+			}
+		}
+		else
+			System.out.print("\n\t\t    NO ENTRIES TO DISPLAY\n");
+	}
+	@Override
+	public void addressBookMenu() 
+	{
+		boolean menuloop=true;
+		while (menuloop==true)
 		{
-			System.out.println(addressBook.get(i));
+			System.out.print( "\n\n\t\t    1 --> Add an entry"
+							+ "\n\t\t    2 --> Edit an entry"
+						 	+ "\n\t\t    3 --> Delete an entry"
+						 	+ "\n\t\t    4 --> Sort Address Book"
+						 	+ "\n\t\t    5 --> Print Address Book"
+							+ "\n\t\t    Any other number to go to previous menu\n");
+			System.out.print("\n\t\t    ENTER YOUR CHOICE :");
+			int choice=Integer.parseInt(takeInput.nextLine());
+			switch(choice)
+			{
+				case 1:
+					this.addPerson();
+				break;
+				case 2:
+					this.editPerson();
+				break;
+				case 3:
+					this.deletePerson();
+				break;
+				case 4:
+					boolean sortloop=true;
+					while(sortloop==true)
+					{	
+						System.out.print("\n\t\t    1 --> Sort by ZipCode\n"
+										+"\n\t\t    2 --> Sort by Last Name\n");
+						System.out.print("\n\t\t    ENTER YOUR CHOICE :");
+						int sortChoice=Integer.parseInt(takeInput.nextLine());
+						switch(sortChoice)
+						{
+							case 1:
+								this.sortAddressBookByZip();
+								sortloop=false;
+							break;
+							case 2:
+								this.sortAddressBookByLastName();
+								sortloop=false;
+							break;
+							default :
+								System.out.print("\n\t\t\tINVALID INPUT");
+						}
+					}
+				break;
+				case 5:
+					this.printAddressBook();
+				break;
+				default :
+					menuloop=false;
+			}	
 		}
 	}
-}
+}	
